70176bcd1b1d570d3bdf8a18fd72a5f85e28b677
albyrex <51365351+albyrex@users.noreply.github.com>
2020-12-24 12:02:38 +0100

    The game is working
    The game is working

--- game.py
+++ game.py
@@ -1,28 +1,22 @@
 import curses
 from random import randint
-import time
-
 
 class Game:
     def __init__(self, n_row, n_column, pause):
-        self.food = []
+        self.food = (2, 5)
         self.end = False
-        self.score = 0
-        self.last_id = 0
+        self.score = 100
         self.pause = pause
         self.n_row = n_row
         self.n_column = n_column
-        self.map = [n_row, n_column]
         self.snake = []
-        self.absolute_direction = "up"
         self.key = -1
+        self.win = ""
 
     def clear(self):
-        self.score = 0
+        self.score = 100
         self.end = False
-        self.food = []
-        self.food.append(2)
-        self.food.append(5)
+        self.food = (2, 5)
         self.snake = []
         self.snake.append((2, 2))
         self.snake.append((3, 2))
@@ -32,97 +26,99 @@ class Game:
         self.snake.append((5, 2))
         self.snake.append((2, 2))
         self.snake.append((6, 2))
-        self.absolute_direction = "up"
-        self.key = -1
 
     def render_init(self):
         curses.initscr()
         win = curses.newwin(self.n_row + 2, self.n_column + 2, 0, 0)
-        curses.curs_set(0)
-        win.nodelay(1)
-        win.timeout(200)
+        win.timeout(self.pause)
+        curses.noecho()
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        win.keypad(True)
+        win.leaveok(True)
         self.win = win
         self.clear()
         self.render()
 
     def render(self):
-        self.win.clear()
-        self.win.border(0)
+        self.win.erase()
+        self.win.border()
         self.win.addstr(0, 2, 'Score : ' + str(self.score) + ' ')
-        self.win.addch(self.food[0], self.food[1], 'p')
-        for i, point in enumerate(self.snake):
-            if i == 0:
-                self.win.addch(point[0], point[1], '#')
-            else:
-                self.win.addch(point[0], point[1], 'o')
-        self.win.getch()
+        self.win.addstr(self.food[0], self.food[1], "ò", curses.color_pair(1))
+        for point in self.snake[1:]:
+            self.win.addch(point[0], point[1], 'o', curses.color_pair(2))
+
+        x = self.snake[0][0] - self.snake[1][0]
+        y = self.snake[0][1] - self.snake[1][1]
+        c = '>'
+        if x == 1 and y == 0:
+            c = 'v'
+        elif x == 0 and y == 1:
+            c = '>'
+        elif x == -1 and y == 0:
+            c = '^'
+        elif x == 0 and y == -1:
+            c = '<'
+        self.win.addch(self.snake[0][0], self.snake[0][1], c, curses.color_pair(2))
 
 
     def generate_food(self):
-        if self.food is False:
-            food = []
+        if self.food == ():
+            food = self.snake[0]
             while food in self.snake:
-                food = [randint(1, self.n_row), randint(1, self.n_column)]
-        return food
+                food = (randint(1, self.n_row), randint(1, self.n_column))
+        self.food = food
 
     def check_collision(self):
-        if (self.snake[0] in self.snake[1:-1] or
+        if (
+                self.snake[0] in self.snake[1:] or
                 self.snake[0][0] == self.n_row + 1 or
-                self.snake[0][0] == 1 or
+                self.snake[0][0] == 0 or
                 self.snake[0][1] == self.n_column + 1 or
-                self.snake[0][1] == 1
+                self.snake[0][1] == 0
         ):
             return True
         else:
             return False
 
-    def generate_new_head(self, key):
-        new_point = [self.snake[0][0], self.snake[0][1]]
-        self.key = key
+    def generate_new_head(self):
         if self.key == curses.KEY_UP:
-            self.absolute_direction = "up"
-            new_point[0] -= 1
+            new_point = (self.snake[0][0] - 1, self.snake[0][1])
         elif self.key == curses.KEY_RIGHT:
-            self.absolute_direction = "right"
-            new_point[1] += 1
+            new_point = (self.snake[0][0], self.snake[0][1] + 1)
         elif self.key == curses.KEY_DOWN:
-            self.absolute_direction = "down"
-            new_point[0] += 1
+            new_point = (self.snake[0][0] + 1, self.snake[0][1])
         elif self.key == curses.KEY_LEFT:
-            self.absolute_direction = "left"
-            new_point[1] -= 1
+            new_point = (self.snake[0][0], self.snake[0][1] - 1)
+        else:
+            x = self.snake[0][0] - self.snake[1][0] + self.snake[0][0]
+            y = self.snake[0][1] - self.snake[1][1] + self.snake[0][1]
+            new_point = (x, y)
 
-        if new_point != self.snake[0]:
-            self.snake.insert(0, new_point)
-        self.key = -1
-
+        self.snake.insert(0, new_point)
 
     def gameover(self):
         self.end = True
-        global stop_thread
-        stop_thread = True
         print("END")
 
-    def tick(self, key):
-        self.generate_new_head(key)
+    def tick(self):
+        self.generate_new_head()
         if self.snake[0] != self.food:
             self.snake.pop()
             self.score -= 1
-        else:
-            self.food = self.generate_food()
-            self.score += 10
+        elif self.snake[0] == self.food:
+            self.food = ()
+            self.generate_food()
+            self.score += 50
+
         collision = self.check_collision()
-        if collision:
+        if collision or self.score < 0:
             self.gameover()
-
-
-
 
     def start(self):
         self.clear()
         while self.end is False:
-            key = self.win.getch()
-            self.tick(key)
-            self.key = -1
             self.render()
-            time.sleep(self.pause)
+            self.key = self.win.getch()
+            self.tick()
--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import time
 from random import randint
 
 if __name__ == '__main__':
-    print("ciao")
-    #game = game.Game(10, 30, 2)
-    #game.render_init()
-    #game.start()
+    game = game.Game(10, 30, 200)
+    game.render_init()
+    game.start()
 
 
 
